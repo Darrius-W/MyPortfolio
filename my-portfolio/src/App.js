c847ff973940320ad40b620e2e1975d21e7d1e4d
Darrius-W <darriuswhite435@gmail.com>
2024-12-28 19:29:19 -0600

    Added styles to left nav

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,40 +1,63 @@
 import './App.css';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
+  const sendMail = () => {
+    window.location.href = "mailto:darriuswhite582@gmail.com?subject=I'd like to connect!"
+  }
+
   return (
     <>
-      <div class="area main-wrapper container">
-        <ul class="circles">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-
-
-        <div class="hero-content">
-          <div class="hero-text container">
-            <h1>HELLO, I AM<br />DARRIUS WHITE.</h1>
-            <p>A software developer passionate about creating
-                interactive front-end designs and delivering comprehensive
-                back-end solutions.
+      <main>
+        <header className="fixed-top">
+          <Nav className="navbar bg-transparent">
+            <div className="container-fluid">
+              <span className='navLeft'>
+                <div className='circle'></div>
+                <a className="name-lnk navbar-brand" href="#">Darrius White</a>
+              </span>
+              <span>
+                <a className="nav-lnks navbar-brand" href="https://github.com/Darrius-W">
+                  <img height="30rem" width="30rem" alt="github-symbol" src="/images/github-mark-white.png"/>
+                </a>
+                <a className="nav-lnks navbar-brand" href="https://www.linkedin.com/in/darrius-white/">
+                  <img height="30rem" width="30rem" src="/images/linkedin-app-white-icon.png" alt="linkedin-symbol"/>
+                </a>
+              </span>
+            </div>
+          </Nav>
+        </header>
+        <section className="home-page d-flex flex-column justify-content-center align-items-center">
+          <video className="bg-video" autoPlay muted loop disablePictureInPicture>
+              <source src="/videos/cctv.mp4" type="video/mp4"/>
+              Your browser does not support the video tag.
+          </video>
+          <div className="main-content">
+            <h2>Hello, I'm Darrius</h2>
+            <h3>I build software solutions for the web.</h3>
+            <p>
+              I'm a software developer specialized in building secure,
+              user-focused full-stack applications that align with business needs.
+              I prioritize intuitive design, robust functionality, and exceptional
+              user experiences.
             </p>
+            <button type="button" onClick={ sendMail } className="contact-btn btn btn-lg btn-outline-light">Get In Touch</button>
           </div>
-          <div class="wrapper">
-            <span id="lnk-wrapper">
-              <a id="contact-lnk" href="mailto:darriuswhite582@gmail.com?subject=I'd like to connect!"><span id="contact-span">Let's Connect</span></a>
-              <a class="my-lnks" href="https://github.com/Darrius-W" target="_blank"><img id="github-lnk" src="/images/icons8-github-90.png" alt="Github-logo" /></a>
-              <a class="my-lnks" href="https://www.linkedin.com/in/darrius-white/" target="_blank"><img id="linkedIn-img" src="/images/icons8-linkedin-70.png" alt="LinkedIn-logo" /></a>
+        </section>
+        <footer className="fixed-bottom d-flex justify-content-center">
+          <div className="footer-sect">
+            <span className="footer-socials gap-3 d-flex justify-content-center">
+              <a className="footer-item navbar-brand" href="https://github.com/Darrius-W">
+                <img id="social-icon" height="30rem" width="30rem" alt="github-symbol" src="/images/github-mark-white.png"/>
+              </a>
+              <a className="footer-item navbar-brand" href="https://www.linkedin.com/in/darrius-white/">
+                <img height="30rem" width="30rem" src="/images/linkedin-app-white-icon.png" alt="linkedin-symbol"/>
+              </a>
             </span>
+            <p className="built-by-tag d-flex justify-content-center">Built by Darrius White</p>
           </div>
-        </div>
-      </div>
+        </footer>
+      </main>
     </>
   );
 }
