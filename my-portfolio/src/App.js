0f63b28e5ffd1b73e37094fd919339cc4687e476
Darrius-W <darriuswhite435@gmail.com>
2025-01-01 13:15:30 -0600

    added active date/time display

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -3,6 +3,51 @@ import Nav from 'react-bootstrap/Nav';
 import { ReactComponent as GitHubIcon } from "./assets/images/github-icon.svg";
 import { ReactComponent as LinkedInIcon } from "./assets/images/linkedin-icon.svg";
 import { ReactComponent as EmailIcon } from "./assets/images/email-icon.svg";
+import React, { useState, useEffect } from 'react';
+
+const TimeDisplay = () => {
+  const [currentTime, setCurrentTime] = useState({
+    date: '',
+    time: ''
+  });
+
+  useEffect(() => {
+    // Function to update the date and time
+    const updateTime = () => {
+      const now = new Date();
+      const date = now.toLocaleDateString('en-US');
+      const time = now.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true, // 24-hour format
+      });
+
+      // Format time (HH:MM:SS)
+      /*const timeOptions = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+      setCurrentTime(now.toLocaleDateString(undefined, timeOptions));*/
+
+      setCurrentTime({ date, time });
+    };
+
+    // update time every second
+    const timer = setInterval(updateTime, 1000);
+
+    // initialize time on mount
+    updateTime();
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return(
+    <div className='time-container'>
+      {/*<p>Your Local Time:</p>*/}
+      <p className="local-date">{ currentTime.date }</p>
+      <p className="local-time">{ currentTime.time }</p>
+      {/*<p className="timezone">Time Zone: { timeZone }</p>*/}
+    </div>
+  );
+};
 
 function App() {
   const sendMail = () => {
@@ -24,6 +69,7 @@ function App() {
                 <a className="name-lnk navbar-brand" href="#">Darrius White</a>
               </span>
               <div className='navRight'>
+               <div className='navRight-extra'></div>
                 <span className='navRight-lnks'>
                   <a href="https://github.com/Darrius-W" target="_blank" className='battery-slab'>
                     <p className="gh-text">GitHub</p>
@@ -46,7 +92,6 @@ function App() {
                   </a>
                   */}
                 </span>
-                <div className='navRight-extra'></div>
               </div>
             </div>
           </Nav>
@@ -81,6 +126,8 @@ function App() {
             <p className="built-by-tag d-flex justify-content-center">Built by Darrius White</p>
           </div>
         </footer>
+        <TimeDisplay />
+        {/*<div className="local-time">00:00:00</div>*/}
       </main>
     </>
   );
