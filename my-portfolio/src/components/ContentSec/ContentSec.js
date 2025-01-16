cdd368b7cc6073b6f79f748293017147cd1641fa
Darrius-W <darriuswhite435@gmail.com>
2025-01-15 19:56:23 -0600

    Improved margins and optimized background image

--- my-portfolio/src/components/ContentSec/ContentSec.js
+++ my-portfolio/src/components/ContentSec/ContentSec.js
@@ -3,11 +3,11 @@ import './ContentSec.css'
 export default function ContentSec(){
     return(
         <section className="home-page d-flex flex-column justify-content-center align-items-center">
-          {/*<video className="bg-video" playsinline webkit-playsinline autoPlay muted loop disablePictureInPicture>
-              <source src="/videos/cctv.webm" type="video/webm"/>
+          {/*<video className="bg-video bg-desktop" playsinline webkit-playsinline autoPlay muted loop disablePictureInPicture>
+              <source src="/videos/cctv.mp4" type="video/mp4"/>
               Your browser does not support the video tag.
           </video>*/}
-          <img className="bg-video" src="/videos/cctv-optimized-30.gif" alt="cctv-static" />
+          <img className="bg-video bg-mobile" src="/videos/cctv-mobile.gif" alt="cctv-static" />
           <div className="main-content">
             <h2>Hello, I'm <span className="myName">Darrius</span></h2>
             <h3>I'm a front-end developer and aspiring full-stack developer.</h3>
