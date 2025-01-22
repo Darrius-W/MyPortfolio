e033edd52aafa1ac558dd0a12cf530017160e364
Darrius-W <darriuswhite435@gmail.com>
2025-01-21 20:56:49 -0600

    Adjusted margins and optimized fonts

--- my-portfolio/src/components/ContentSec/ContentSec.js
+++ my-portfolio/src/components/ContentSec/ContentSec.js
@@ -7,10 +7,10 @@ export default function ContentSec(){
               <source src="/videos/cctv.mp4" type="video/mp4"/>
               Your browser does not support the video tag.
           </video>*/}
-          <img className="bg-video bg-mobile" src="https://raw.githubusercontent.com/Darrius-W/MyPortfolio/refs/heads/development/my-portfolio/public/videos/cctv-mobile.gif" alt="cctv-static" />
+          <img className="bg-video bg-mobile" src="https://raw.githubusercontent.com/Darrius-W/MyPortfolio/refs/heads/main/my-portfolio/public/videos/cctv-mobile.gif" alt="cctv-static" />
           <div className="main-content">
-            <h2>Hello, I'm <span className="myName">Darrius</span></h2>
-            <h3>I'm a front-end developer and aspiring full-stack developer.</h3>
+            <h1>Hello, I'm <span className="myName">Darrius</span></h1>
+            <h2>I'm a front-end developer and aspiring full-stack developer.</h2>
             <p className='content-text'>
               I specialize in developing web applications with a focus on creating
               intuitive UI designs that bring creative visions to life. I prioritize
